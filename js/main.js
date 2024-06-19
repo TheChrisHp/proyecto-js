8a9022be84ae637fe13efbb195a5c1ae54f508f2
Christian Fortes <lucasesmid@gmail.com>
2024-06-18 21:06:41 -0300

    Implementación de SCSS, arreglos estéticos y demás

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ let edad = 21
 console.log(nombre)
 console.log(apellido)
 console.log(edad)
+
+//CONDICIONALES
+
+// let nom = prompt("Ingrese su nombre: ")
+// let ap = prompt("Ingrese su apellido: ")
+// let ed = Number(prompt("Ingrese su edad: "))
+
+// if (nom == nombre && ap == apellido && edad == 21){
+//     alert("Bienvenido " + nombre+ " " + apellido+ " " + " ✅")
+// }else{
+//     alert("Usuario no reconocido. ⛔")
+// }
+
+// let vari = false
+
+// if(vari){
+//     //true
+//     console.log("Es verdadero")
+// }else{
+//     //false
+//     console.log("Es falso")
+// }
